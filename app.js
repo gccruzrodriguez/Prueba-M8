dedb7a873a7d61e9bbdea7c851faf919bae8b74b
gabrielacmrj <gabrielacruz@gmx.us>
2025-03-10 20:37:07 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");  
+const app = express(); 
+const port = 3000;
+
+
+app.use(express.static("public"));
+
+
+app.get("/contact", (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'public', 'contact.html')); 
+});
+
+app.get("/", (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'public', 'home.html')); 
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en: http://localhost:${port}`);
+});
+
